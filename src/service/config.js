3f8294fe5ae848e990c74dd52d42e48667c3033d
longdv ph29867 <emchuaroinhe@gmail.com>
2024-02-15 17:55:36 +0700

    update token

--- src/service/config.js
+++ src/service/config.js
@@ -1,15 +1,16 @@
 import axios from "axios";
 export const BASE_URL = "https://movienew.cybersoft.edu.vn/";
 
-const TokenCybersoft = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA0MSIsIkhldEhhblN0cmluZyI6IjEyLzA5LzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY5NDQ3NjgwMDAwMCIsIm5iZiI6MTY2NTI0ODQwMCwiZXhwIjoxNjk0NjI0NDAwfQ.SUELcPShU58ZkNS3CbFDhM02KMzll9j00ndjVSaiJ8Q";
+const TokenCybersoft =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAwMSIsIkhldEhhblN0cmluZyI6IjMwLzA5LzIwMzEiLCJIZXRIYW5UaW1lIjoiMTk0ODQ5MjgwMDAwMCIsIm5iZiI6MTYwMTIyNjAwMCwiZXhwIjoxOTQ4NjQwNDAwfQ.4l-eTzlgVnFczfvc2Or7BNPOcaesY3Kwc8RoNm-o-6M";
 
 export let configHeaders = () => {
-    return {
-        TokenCybersoft: TokenCybersoft,
-    }
-}
+  return {
+    TokenCybersoft: TokenCybersoft,
+  };
+};
 
 export const https = axios.create({
-    baseURL: BASE_URL,
-    headers: configHeaders(),
-})
+  baseURL: BASE_URL,
+  headers: configHeaders(),
+});
--- src/service/movieService.js
+++ src/service/movieService.js
@@ -1,27 +1,25 @@
 import { https } from "./config";
 
-
 export const movieServ = {
-    getMovieList: () => {
-
-        return https.get("/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP04");
-    },
-    getMovieRap: () => {
-        return https.get("/api/QuanLyRap/LayThongTinHeThongRap?maNhom=GP04")
-    },
-    getMovieSchelude: (id) => {
-        return https.get(`/api/QuanLyRap/LayThongTinLichChieuPhim?MaPhim=${id}`)
-    },
-    getMovieList: () => {
-        return https.get("/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP07");
-    },
-    getMovieByTheater: () => {
-        return https.get("/api/QuanLyRap/LayThongTinLichChieuHeThongRap");
-    },
-    getMovieDetailSchedule: (id) => {
-        return https.get(`/api/QuanLyRap/LayThongTinLichChieuPhim?MaPhim=${id}`);
-    },
-    getMovieBooking: (id) => {
-        return https.get(`/api/QuanLyDatVe/LayDanhSachPhongVe?MaLichChieu=${id}`);
-    }
-}
+  getMovieList: () => {
+    return https.get("/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP04");
+  },
+  getMovieRap: () => {
+    return https.get("/api/QuanLyRap/LayThongTinHeThongRap?maNhom=GP04");
+  },
+  getMovieSchelude: (id) => {
+    return https.get(`/api/QuanLyRap/LayThongTinLichChieuPhim?MaPhim=${id}`);
+  },
+  getMovieList: () => {
+    return https.get("/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP04");
+  },
+  getMovieByTheater: () => {
+    return https.get("/api/QuanLyRap/LayThongTinLichChieuHeThongRap");
+  },
+  getMovieDetailSchedule: (id) => {
+    return https.get(`/api/QuanLyRap/LayThongTinLichChieuPhim?MaPhim=${id}`);
+  },
+  getMovieBooking: (id) => {
+    return https.get(`/api/QuanLyDatVe/LayDanhSachPhongVe?MaLichChieu=${id}`);
+  },
+};
